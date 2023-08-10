b4dea740b58526b0e89a8e89bda3615bf47e5521
OfficialDelta <51007646+TSLARoadster@users.noreply.github.com>
2023-08-09 23:04:02 -0400

    "Update project dependencies and refactor AI logic"
    Updated the versions of 'python-dotenv', 'sqlalchemy', 'numpy', and 'asyncio' in requirements.txt to improve compatibility and stability.
    
    Refactored 'oxygen_ai.py' for improved code readability and maintainability. The refactoring included reordering import statements, reformatting function definitions and calls for better readability, and adding tqdm to provide visual feedback for lengthy operations. It also involved reorganizing data processing logic and updating variable names for better comprehension. This refactoring will make future changes and debugging easier.

--- oxygen_ai.py
+++ oxygen_ai.py
@@ -1,17 +1,18 @@
-import asyncio
-
-from sqlalchemy import Float, String
-
-
 def main():
-    print('Started AI Process')
+    print("Started AI Process")
 
     import numpy
     from psycopg2.extensions import register_adapter, AsIs
     from sqlalchemy import Table, Column, Integer, MetaData
     from sqlalchemy.sql.type_api import UserDefinedType
     import json
-    from memphis import Memphis, MemphisError, MemphisConnectError, MemphisHeaderError, MemphisSchemaError
+    from memphis import (
+        Memphis,
+        MemphisError,
+        MemphisConnectError,
+        MemphisHeaderError,
+        MemphisSchemaError,
+    )
     from sqlalchemy import create_engine, func, text
     from dotenv import load_dotenv
     import os
@@ -21,6 +22,9 @@ def main():
     import pickle
     from typing import Union
     import numpy as np
+    import asyncio
+    from sqlalchemy import Float, String
+    from tqdm import tqdm
 
     load_dotenv()
 
@@ -28,10 +32,10 @@ def main():
     username = os.getenv("MEMPHIS_USERNAME")
     password = os.getenv("MEMPHIS_PASSWORD")
     account_id = os.getenv("MEMPHIS_ACCOUNT_ID")
-    pg_user = os.getenv('PG_USER')
-    pg_password = os.getenv('PG_PASSWORD')
-    pg_host = os.getenv('PG_HOST')
-    pg_dbname = os.getenv('PG_DBNAME')
+    pg_user = os.getenv("PG_USER")
+    pg_password = os.getenv("PG_PASSWORD")
+    pg_host = os.getenv("PG_HOST")
+    pg_dbname = os.getenv("PG_DBNAME")
 
     # hacky solution for numpy64
     def addapt_numpy_float64(numpy_float64):
@@ -43,9 +47,7 @@ def main():
     register_adapter(numpy.float64, addapt_numpy_float64)
     register_adapter(numpy.int64, addapt_numpy_int64)
 
-    conn = create_engine(
-        f"postgresql://{pg_user}:{pg_password}@{pg_host}/{pg_dbname}"
-    )
+    conn = create_engine(f"postgresql://{pg_user}:{pg_password}@{pg_host}/{pg_dbname}")
 
     classifier = pipeline("sentiment-analysis", model="./blaze_nlp")
     lstm_model = keras.models.load_model("lstm_model.h5")
@@ -76,9 +78,13 @@ def main():
         :return: A sentiment score based on the model's output. Positive for 'yes_fire', negative for 'no_fire'.
         """
 
-        return model_output["score"] * (1 if model_output["label"] == "yes_fire" else -1)
+        return model_output["score"] * (
+            1 if model_output["label"] == "yes_fire" else -1
+        )
 
-    def predict_fire_from_temp(temperatures: Union[list[int], list[list[int]]]) -> list[dict]:
+    def predict_fire_from_temp(
+        temperatures: Union[list[int], list[list[int]]]
+    ) -> list[dict]:
         """
         Predicts fire based on the given temperatures.
 
@@ -105,8 +111,8 @@ def main():
         return output
 
     def predict_fire(
-            tweets: Union[list[str], list[list[str]]],
-            temperatures: Union[list[int], list[list[int]]],
+        tweets: Union[list[str], list[list[str]]],
+        temperatures: Union[list[int], list[list[int]]],
     ) -> list[float]:
         """
         Predicts fire based on the given tweets and temperatures.
@@ -122,7 +128,9 @@ def main():
 
         if not tweets:
             # Handle case when tweets is an empty list
-            flat_temperatures = [temp for temp_batch in temperatures for temp in temp_batch]
+            flat_temperatures = [
+                temp for temp_batch in temperatures for temp in temp_batch
+            ]
             temperature_fire = predict_fire_from_temp(flat_temperatures)
             return [derive_sentiment(temp) for temp in temperature_fire]
 
@@ -137,7 +145,9 @@ def main():
             tweet_batch_indices.append(len(flat_tweets))
 
         # Flattening temperatures and storing their batch indices
-        flat_temperatures = [temp for temp_batch in temperatures for temp in temp_batch]
+        flat_temperatures = [
+            temp for temp_batch in temperatures for temp in temp_batch
+        ]
         temperature_batch_indices = [0]
         for i in range(len(temperatures)):
             temperature_batch_indices.append(
@@ -164,7 +174,9 @@ def main():
                 sentiment = derive_sentiment(tweet)
                 average_tweet_sentiment += sentiment * (0.2 if sentiment < 0 else 1)
 
-            average_tweet_sentiment = average_tweet_sentiment / len(tweet_batch_result)
+            average_tweet_sentiment = average_tweet_sentiment / len(
+                tweet_batch_result
+            )
             average_tweet_sentiment = round(average_tweet_sentiment)
 
             temperature_sentiment = derive_sentiment(temperature_batch_result)
@@ -172,14 +184,16 @@ def main():
             if not average_tweet_sentiment:
                 output.append(temperature_sentiment)
             else:
-                output.append(average_tweet_sentiment * 0.4 + temperature_sentiment * 0.6)
+                output.append(
+                    average_tweet_sentiment * 0.4 + temperature_sentiment * 0.6
+                )
 
         return output
 
     def rolling_samples(start_day, series, window=7):
         output = []
         for i in range(window, len(series)):
-            sample = series[i - window: i].tolist()
+            sample = series[i - window : i].tolist()
             output.append((sample, i + start_day - 1))
         return output
 
@@ -187,39 +201,41 @@ def main():
         return sample["tweets"], sample["temperature"]
 
     def parse_key(s: str):
-        day_str, _, coordinates_str = s.split('(')
+        day_str, coordinates_str = s.split("(")
         day = int(day_str)
-        x_str, y_str = coordinates_str.strip(')').strip('(').split(',')
+        x_str, y_str = coordinates_str.strip(")").strip("(").split(",")
         x = int(x_str)
         y = int(y_str)
         return day, x, y
 
-    async def ai_model(batched_tweets, batched_temperatures, batched_keys):
+    async def ai_model(batched_ids, data):
         memphis = Memphis()
         station_name = "zakar-fire-predictions"
         try:
-            await memphis.connect(host=host, username=username, password=password, account_id=account_id)
-            producer = await memphis.producer(station_name=f"{station_name}",
-                                              producer_name=f"{station_name}-producer")  # you can send the message parameter as dict as well
-            for i in range(len(batched_tweets)):
-                print(f"Batch {i + 1}/{len(batched_tweets)}")
-                predictions = predict_fire(batched_tweets[i], batched_temperatures[i])
+            await memphis.connect(
+                host=host, username=username, password=password, account_id=account_id
+            )
+            producer = await memphis.producer(
+                station_name=f"{station_name}", producer_name=f"{station_name}-producer"
+            )
+            for i in range(len(batched_ids)):
+                print(f"Processing batch {i + 1} of {len(batched_ids)}")
+                batch = batched_ids[i]
+
+                input_samples = data[data["ID"].isin(batch)]
+                input_tweets = input_samples["tweets"].tolist()
+                input_temperatures = input_samples["temperature"].tolist()
+
+                predictions = predict_fire(input_tweets, input_temperatures)
                 msgs = []
-                highest_score = {
-                    'score': 0,
-                    'day': 0,
-                    'x': -1,
-                    'y': -1
-                }
-                lowest_score = {
-                    'score': 1,
-                    'day': 0,
-                    'x': -1,
-                    'y': -1
-                }
-                for j, key in enumerate(batched_keys[i]):
+                highest_score = {"score": -1, "day": 0, "x": -1, "y": -1}
+                lowest_score = {"score": 1, "day": 0, "x": -1, "y": -1}
+
+                for j in range(len(predictions)):
+                    prediction = predictions[j]
+                    key = batch[j]
                     day, x, y = parse_key(key)
-                    if predictions[j] > 0.7:
+                    if prediction > 0.7:
                         msg = {
                             "day": day,
                             "geospatial_x": x,
@@ -228,31 +244,38 @@ def main():
 
                         await producer.produce(bytearray(json.dumps(msg), "utf-8"))
 
-                        msg['score'] = predictions[j]
+                        msg["score"] = prediction
                         msgs.append(msg)
 
-                    if predictions[j] > highest_score['score']:
-                        highest_score['score'] = predictions[j]
-                        highest_score['day'] = day
-                        highest_score['x'] = x
-                        highest_score['y'] = y
-                        highest_score['j'] = j
-                    elif predictions[j] < lowest_score['score']:
-                        lowest_score['score'] = predictions[j]
-                        lowest_score['day'] = day
-                        lowest_score['x'] = x
-                        lowest_score['y'] = y
-                        lowest_score['j'] = j
+                    if prediction > highest_score["score"]:
+                        highest_score["score"] = predictions[j]
+                        highest_score["day"] = day
+                        highest_score["x"] = x
+                        highest_score["y"] = y
+                        highest_score["j"] = j
+                    elif prediction < lowest_score["score"]:
+                        lowest_score["score"] = predictions[j]
+                        lowest_score["day"] = day
+                        lowest_score["x"] = x
+                        lowest_score["y"] = y
+                        lowest_score["j"] = j
 
                 if len(msgs) > 0:
                     postgres_egress(msgs)
 
-                print(f"Highest score: {highest_score['score']} on day {highest_score['day']} at ({highest_score['x']}, {highest_score['y']})")
-                print(f"Lowest score: {lowest_score['score']} on day {lowest_score['day']} at ({lowest_score['x']}, {lowest_score['y']})")
-
-        except (MemphisError, MemphisConnectError, MemphisHeaderError, MemphisSchemaError) as e:
+                print(
+                    f"Highest score: {highest_score['score']} on day {highest_score['day']} at ({highest_score['x']}, {highest_score['y']})"
+                )
+                print(
+                    f"Lowest score: {lowest_score['score']} on day {lowest_score['day']} at ({lowest_score['x']}, {lowest_score['y']})"
+                )
+        except (
+            MemphisError,
+            MemphisConnectError,
+            MemphisHeaderError,
+            MemphisSchemaError,
+        ) as e:
             print(e)
-
         finally:
             await memphis.close()
 
@@ -261,18 +284,15 @@ def main():
         insert_statement = ai_firealerts_production.insert()
 
         for msg in msg_list:
-            day = msg['day']
-            x = msg['geospatial_x']
-            y = msg['geospatial_y']
-            score = msg['score']
+            day = msg["day"]
+            x = msg["geospatial_x"]
+            y = msg["geospatial_y"]
+            score = msg["score"]
             id = f"{day}({x},{y})"
 
-            value_list.append({
-                'id': id,
-                'day': day,
-                'xy': func.point(x, y),
-                'score': score
-            })
+            value_list.append(
+                {"id": id, "day": day, "xy": func.point(x, y), "score": score}
+            )
 
         if len(value_list) > 0:
             with conn.connect() as connection:
@@ -282,75 +302,75 @@ def main():
 
     metadata = MetaData()
 
-    ai_firealerts_production = Table('ai_fire_alerts_production', metadata,
-                                     Column('id', String, primary_key=True),
-                                     Column('day', Integer),
-                                     Column('xy', Point),
-                                     Column('score', Float))
+    ai_firealerts_production = Table(
+        "ai_fire_alerts_production",
+        metadata,
+        Column("id", String, primary_key=True),
+        Column("day", Integer),
+        Column("xy", Point),
+        Column("score", Float),
+    )
 
     metadata.create_all(conn)
 
-    print("Temperature starting")
-    temp_readings = pd.read_sql_query(text("SELECT * FROM public.temp_readings_production"), conn)
+    print("Loading temperature")
+    temp_readings = pd.read_sql_query(
+        text("SELECT * FROM public.temp_readings_production"), conn
+    )
 
     temp_readings = temp_readings.sort_values("day")
 
     start_day = temp_readings.iloc[0]["day"]
-    print("Temperature done")
     samples = []
     print("Rolling samples")
-    for name, group in temp_readings.groupby("xy"):
+    for name, group in tqdm(temp_readings.groupby("xy")):
         group = group.sort_values("day")
         temps = rolling_samples(start_day, group["temperature"].set_axis(group["day"]))
 
         for temp, i in temps:
-            samples.append({
-                "day": i,
-                "xy": name,
-                "temp": temp
-            })
-    print("Sorting samples")
-    samples = pd.DataFrame.from_dict(sorted(samples, key=lambda x: x["day"]))
-    print("Sorting done")
+            samples.append({"day": i, "xy": name, "temp": temp})
+
+    samples = pd.DataFrame(samples)
 
     print("Loading tweets")
     tweets = pd.read_sql_query(text("SELECT * FROM public.tweets_production"), conn)
 
-    merged_data = pd.merge(samples, tweets, how='left', on=['xy', 'day']).drop(["id", "score"], axis=1).fillna("")
+    print("Merging data")
+    merged_data = (
+        pd.merge(samples, tweets, how="left", on=["xy", "day"])
+        .drop(["id", "score"], axis=1)
+        .fillna("")
+    )
 
-    complete_samples = merged_data.groupby(['xy', 'day']).agg(
-        # include other columns as needed
-        tweets=pd.NamedAgg(column='content', aggfunc=list),
-        temperature=pd.NamedAgg(column='temp', aggfunc='first')
-    ).reset_index()
-
-    complete_samples = complete_samples.sort_values("day")
-
-    complete_samples = complete_samples.to_dict(orient='records')
-
+    complete_samples = (
+        merged_data.groupby(["xy", "day"])
+        .agg(
+            # include other columns as needed
+            tweets=pd.NamedAgg(column="content", aggfunc=list),
+            temperature=pd.NamedAgg(column="temp", aggfunc="first"),
+        )
+        .reset_index()
+    )
+    complete_samples[["x", "y"]] = (
+        complete_samples["xy"].str.strip("()").str.split(",", expand=True).astype(int)
+    )
+    complete_samples = complete_samples.drop("xy", axis=1)
+    complete_samples["ID"] = (
+        complete_samples["day"].astype(str)
+        + "("
+        + complete_samples["x"].astype(str)
+        + ","
+        + complete_samples["y"].astype(str)
+        + ")"
+    )
+    complete_samples = complete_samples.sort_values(["day", "x", "y"])
 
     # Define batch size
     batch_size = 128
 
-    # print(test_samples[0])
-    data = {}
-    for sample in complete_samples:
-        key = f"{sample['day']}({sample['xy']})"
-        items = [item for sublist in map(dict_to_example, [sample]) for item in sublist]
-        data[key] = items
+    ids = complete_samples["ID"].values
 
-    # Split the data into batches
-    keys = list(data.keys())
-    batched_keys = []
-    batched_tweets = []
-    batched_temperatures = []
-    for i in range(0, len(keys), batch_size):
-        batch_keys = keys[i:i + batch_size]
-        batch_tweets = [data[key][0] for key in keys[i:i + batch_size]]
-        batch_temperatures = [data[key][1] for key in keys[i:i + batch_size]]
-        batched_keys.append(batch_keys)
-        batched_tweets.append(batch_tweets)
-        batched_temperatures.append(batch_temperatures)
+    batches = [ids[i : i + batch_size] for i in range(0, len(ids), batch_size)]
 
     # Call predict_fire on each batch and map the outputs to the keys
-    asyncio.run(ai_model(batched_tweets, batched_temperatures, batched_keys))
+    asyncio.run(ai_model(batches, complete_samples))
